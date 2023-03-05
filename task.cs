d8a8266a997acc6479de8b5e197e669ac0cec0ed
IradaG <iradagulaliyeva18@mail.ru>
2023-03-06 00:30:50 +0400

    initialize Change Array size task

---
+++ task.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+bool flag = true;
+int increment = 0;
+Console.WriteLine("Please enter the size of the array");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[sizeArray];
+do
+{
+    Console.WriteLine("Do you want to continue? ");
+    string answer = Console.ReadLine();
+    int element = Convert.ToInt32(Console.ReadLine());
+    if (answer == "y" || answer == "Y")
+    {
+    Console.WriteLine("Enter element ");
+    Console.WriteLine(element);
+    numbers[increment] = Convert.ToInt32(Console.ReadLine());
+    numbers[increment] = element;
+    increment++;
+    }
+    else if(answer == "no")
+    {
+
+        flag = false;
+        Console.WriteLine(".......exiting");
+        break;
+    }
+
+} while (flag);
